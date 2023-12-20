4987ea54656d70060ba6216b554a57dd20c074be
michael wahyudin <michaelwahyudin90@gmail.com>
2023-12-20 10:54:39 +0700

    noice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,10 @@ import MultiStepForm from './MultiStepForm';
 
 const App = () => {
   return (
-    <div className="App">
-      <MultiStepForm />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
+        <MultiStepForm />
+      </div>
     </div>
   );
 };
