ad3ccac0f0838d0e5dfc09416fb167d889502385
michael wahyudin <michaelwahyudin90@gmail.com>
2023-12-20 12:26:30 +0700

    update css

--- my-app/src/postcss.config.js
+++ my-app/src/postcss.config.js
@@ -1,8 +1,8 @@
 // postcss.config.js
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
-  
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // ... other plugins
+  ],
+};
